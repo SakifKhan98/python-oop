2776727aee518a942c5fda1f708ce1af2043aa50
SakifKhan98 <sakifkhan98@gmail.com>
2022-02-14 07:02:05 +0600

    Static method implemented

---
+++ main.py
@@ -0,0 +1,79 @@
+import csv
+
+
+class Item:
+    pay_rate = 0.8  # The pay rate after 20% discount
+    all = []
+
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        # We will count out the floats that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+
+print(Item.is_integer(10))
+# Item.instantiate_from_csv()
+# print(Item.all)
+
+# print(Item.all)
+# for instance in Item.all:
+#     print(instance.name)
+# item1 = Item("Phone", 100, 5)
+# item2 = Item("Laptop", 1000, 3)
+# item2.pay_rate = 0.7
+# item1.apply_discount()
+# item2.apply_discount()
+#
+# total_price_1 = item1.calculate_total_price()
+# total_price_2 = item2.calculate_total_price()
+#
+# print("Total Price of Mobile =>", total_price_1)
+# print("Total Price of Laptop =>", total_price_2)
+#
+# print(Item.__dict__)  # All the attributes for Class level
+# print(item1.__dict__)  # All the attributes for Instance level
+
+
+
